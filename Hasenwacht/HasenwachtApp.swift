f1db35acb2293c634dc5f855bf6571a870b5350f
Marcel Felder <marcel.felder@bluewin.ch>
2026-05-04 19:59:02 +0200

    Initial Commit

---
+++ Hasenwacht/HasenwachtApp.swift
@@ -0,0 +1,17 @@
+//
+//  HasenwachtApp.swift
+//  Hasenwacht
+//
+//  Created by Marcel Felder on 04.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct HasenwachtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
